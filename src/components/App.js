900f44b87d061dd75f3d575064d1058550433f9d
Nuria Castaño <codesalnuria@gmail.com>
2023-03-06 12:37:28 +0100

    add const inputs

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
 import '../styles/App.scss';
 import cover from '../images/cover.jpeg';
 import user from '../images/user.jpeg';
+import {useState} from 'react';
 // import logo from '../images/logo-adalab.png';
 // import cover2 from '../images/cover_2.jpeg';
 
 
 function App() {
+  const [name, setName] = useState('');
+  const [slogan, setSlogan] = useState('');
+  const [repo, setRepo] = useState('');
+  const [demo, setDemo] = useState('');
+  const [technologies, setTechnologies] = useState('');
+  const [desc, setDesc] = useState('');
+  const [autor, setAutor] = useState('');
+  const [job, setJob] = useState('');
+
+  const handleInput = (ev) => {
+    const inputValue= ev.target.value;
+    const inputName = ev.target.name;
+    if(inputName==="name"){
+      setName(inputValue);
+    } else if(inputName==="slogan"){
+      setSlogan(inputValue);
+    } else if(inputName==="repo"){
+      setRepo(inputValue);
+    } else if(inputName==="demo"){
+      setDemo(inputValue);
+    } else if(inputName==="technologies"){
+      setTechnologies(inputValue);
+    } else if(inputName==="desc"){
+      setDesc(inputValue);
+    } else if(inputName==="autor"){
+      setAutor(inputValue);
+    } else if(inputName==="job"){
+      setJob(inputValue);
+    }
+  }
+  
+
+
   return <div className="container">
       <header className="header">
         <p className="text">Proyectos Molones</p>
@@ -19,23 +53,19 @@ function App() {
               <p className="subtitle">Personal Project Card</p>
               <hr className="line" />
 
-              <h2 className="title">Elegant Workspace</h2>
-              <p className="slogan">Diseños Exclusivos</p>
-              <p className="desc">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Libero, delectus? Voluptates at hic aliquam porro ad suscipit
-                harum laboriosam saepe earum doloribus aperiam, ullam culpa
-                accusantium placeat odit corrupti ipsum!
+              <h2 className="title">{name || "Elegant Workspace"}</h2>
+              <p className="slogan">{slogan || "Diseños Exclusivos"}</p>
+              <p className="desc">{desc || "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Libero, delectus? Voluptates at hic aliquam porro ad suscipi harum laboriosam saepe earum doloribus aperiam, ullam culpa accusantium placeat odit corrupti ipsum!"}
               </p>
               <section className="technologies">
-                <p className="text">React JS, MongoDB</p>
+                <p className="text">{technologies || "React JS, MongoDB"}</p>
               </section>
             </section>
 
             <section className="info-autor">
               <img className="image" src={user} alt="" />
-              <p className="job">Full Stack Developer</p>
-              <p className="name">Emmelie Björklund</p>
+              <p className="job">{job || "Full Stack Developer"}</p>
+              <p className="name">{autor || "Emmelie Björklund"}</p>
             </section>
           </section>
         </section>
@@ -55,6 +85,8 @@ function App() {
               placeholder="Nombre del proyecto"
               name="name"
               id="name"
+              value={name}
+              onChange={handleInput}
             />
             <input
               className="input"
@@ -62,6 +94,8 @@ function App() {
               name="slogan"
               id="slogan"
               placeholder="Slogan"
+              value={slogan}
+              onChange={handleInput}
             />
             <input
               className="input"
@@ -69,6 +103,8 @@ function App() {
               name="repo"
               id="repo"
               placeholder="Repo"
+              value={repo}
+              onChange={handleInput}
             />
             <input
               className="input"
@@ -76,6 +112,8 @@ function App() {
               placeholder="Demo"
               name="demo"
               id="demo"
+              value={demo}
+              onChange={handleInput}
             />
             <input
               className="input"
@@ -83,6 +121,8 @@ function App() {
               placeholder="Tecnologías"
               name="technologies"
               id="technologies"
+              value={technologies}
+              onChange={handleInput}
             />
             <textarea
               className="textarea"
@@ -90,6 +130,8 @@ function App() {
               placeholder="Descripción"
               name="desc"
               id="desc"
+              value={desc}
+              onChange={handleInput}
             ></textarea>
           </fieldset>
 
@@ -105,6 +147,8 @@ function App() {
               placeholder="Nombre"
               name="autor"
               id="autor"
+              value={autor}
+              onChange={handleInput}
             />
             <input
               className="input"
@@ -112,6 +156,8 @@ function App() {
               placeholder="Trabajo"
               name="job"
               id="job"
+              value={job}
+              onChange={handleInput}
             />
           </fieldset>
 
