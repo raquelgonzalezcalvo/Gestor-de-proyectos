9a5958540e70e4b30b749a6fb23c3cb691ca5c03
Nuria Castaño <codesalnuria@gmail.com>
2023-03-06 13:58:19 +0100

    add new html

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,44 @@
-import '../styles/App.scss';
-import cover from '../images/cover.jpeg';
-import user from '../images/user.jpeg';
-import {useState} from 'react';
+import "../styles/App.scss";
+import cover from "../images/cover.jpeg";
+import user from "../images/user.jpeg";
+import { useState } from "react";
 // import logo from '../images/logo-adalab.png';
 // import cover2 from '../images/cover_2.jpeg';
 
-
 function App() {
-  const [name, setName] = useState('');
-  const [slogan, setSlogan] = useState('');
-  const [repo, setRepo] = useState('');
-  const [demo, setDemo] = useState('');
-  const [technologies, setTechnologies] = useState('');
-  const [desc, setDesc] = useState('');
-  const [autor, setAutor] = useState('');
-  const [job, setJob] = useState('');
+  const [name, setName] = useState("");
+  const [slogan, setSlogan] = useState("");
+  const [repo, setRepo] = useState("");
+  const [demo, setDemo] = useState("");
+  const [technologies, setTechnologies] = useState("");
+  const [desc, setDesc] = useState("");
+  const [autor, setAutor] = useState("");
+  const [job, setJob] = useState("");
 
   const handleInput = (ev) => {
-    const inputValue= ev.target.value;
+    const inputValue = ev.target.value;
     const inputName = ev.target.name;
-    if(inputName==="name"){
+    if (inputName === "name") {
       setName(inputValue);
-    } else if(inputName==="slogan"){
+    } else if (inputName === "slogan") {
       setSlogan(inputValue);
-    } else if(inputName==="repo"){
+    } else if (inputName === "repo") {
       setRepo(inputValue);
-    } else if(inputName==="demo"){
+    } else if (inputName === "demo") {
       setDemo(inputValue);
-    } else if(inputName==="technologies"){
+    } else if (inputName === "technologies") {
       setTechnologies(inputValue);
-    } else if(inputName==="desc"){
+    } else if (inputName === "desc") {
       setDesc(inputValue);
-    } else if(inputName==="autor"){
+    } else if (inputName === "autor") {
       setAutor(inputValue);
-    } else if(inputName==="job"){
+    } else if (inputName === "job") {
       setJob(inputValue);
     }
-  }
-  
+  };
 
-
-  return <div className="container">
+  return (
+    <div className="container">
       <header className="header">
         <p className="text">Proyectos Molones</p>
       </header>
@@ -48,136 +46,157 @@ function App() {
         <section className="preview">
           <img className="image" src={cover} alt="" />
 
-          <section className="autor">
-            <section className="info-project">
+          <article className="autor">
+            <div className="info-project">
               <p className="subtitle">Personal Project Card</p>
               <hr className="line" />
 
               <h2 className="title">{name || "Elegant Workspace"}</h2>
               <p className="slogan">{slogan || "Diseños Exclusivos"}</p>
-              <p className="desc">{desc || "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Libero, delectus? Voluptates at hic aliquam porro ad suscipi harum laboriosam saepe earum doloribus aperiam, ullam culpa accusantium placeat odit corrupti ipsum!"}
+              <p className="desc">
+                {desc ||
+                  "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Libero, delectus? Voluptates at hic aliquam porro ad suscipi harum laboriosam saepe earum doloribus aperiam, ullam culpa accusantium placeat odit corrupti ipsum!"}
               </p>
-              <section className="technologies">
+              <div className="technologies">
                 <p className="text">{technologies || "React JS, MongoDB"}</p>
-              </section>
-            </section>
+              </div>
+              <div>
+                <a href={repo} target="_blank" rel="noreferrer">
+                  <i class="fa-brands fa-github-alt"></i>
+                </a>
+                <a href={demo} target="_blank" rel="noreferrer">
+                  <i class="fa-solid fa-globe"></i>
+                </a>
+              </div>
+            </div>
 
-            <section className="info-autor">
+            <div className="info-autor">
               <img className="image" src={user} alt="" />
               <p className="job">{job || "Full Stack Developer"}</p>
               <p className="name">{autor || "Emmelie Björklund"}</p>
-            </section>
-          </section>
+            </div>
+          </article>
         </section>
-
-        <section className="form">
+        <section>
           <h2 className="title">Información</h2>
+          <form className="form">
+            <fieldset className="project">
+              <legend className="ask-info subtitle">
+                Cuéntanos sobre el proyecto
+                <hr className="line" />
+              </legend>
+              <input
+                className="input"
+                type="text"
+                placeholder="Nombre del proyecto"
+                name="name"
+                id="name"
+                value={name}
+                onChange={handleInput}
+              />
+              <input
+                className="input"
+                type="text"
+                name="slogan"
+                id="slogan"
+                placeholder="Slogan"
+                value={slogan}
+                onChange={handleInput}
+              />
+              <input
+                className="input"
+                type="text"
+                name="repo"
+                id="repo"
+                placeholder="Repo"
+                value={repo}
+                onChange={handleInput}
+              />
+              <input
+                className="input"
+                type="text"
+                placeholder="Demo"
+                name="demo"
+                id="demo"
+                value={demo}
+                onChange={handleInput}
+              />
+              <input
+                className="input"
+                type="text"
+                placeholder="Tecnologías"
+                name="technologies"
+                id="technologies"
+                value={technologies}
+                onChange={handleInput}
+              />
+              <textarea
+                className="textarea"
+                type="text"
+                placeholder="Descripción"
+                name="desc"
+                id="desc"
+                value={desc}
+                onChange={handleInput}
+              ></textarea>
+            </fieldset>
 
-          <section className="ask-info">
-            <p className="subtitle">Cuéntanos sobre el proyecto</p>
-            <hr className="line" />
-          </section>
+            <fieldset className="autor">
+              <legend className="ask-info subtitle">
+                Cuéntanos sobre la autora
+                <hr className="line" />
+              </legend>
+              <input
+                className="input"
+                type="text"
+                placeholder="Nombre"
+                name="autor"
+                id="autor"
+                value={autor}
+                onChange={handleInput}
+              />
+              <input
+                className="input"
+                type="text"
+                placeholder="Trabajo"
+                name="job"
+                id="job"
+                value={job}
+                onChange={handleInput}
+              />
+            </fieldset>
 
-          <fieldset className="project">
-            <input
-              className="input"
-              type="text"
-              placeholder="Nombre del proyecto"
-              name="name"
-              id="name"
-              value={name}
-              onChange={handleInput}
-            />
-            <input
-              className="input"
-              type="text"
-              name="slogan"
-              id="slogan"
-              placeholder="Slogan"
-              value={slogan}
-              onChange={handleInput}
-            />
-            <input
-              className="input"
-              type="text"
-              name="repo"
-              id="repo"
-              placeholder="Repo"
-              value={repo}
-              onChange={handleInput}
-            />
-            <input
-              className="input"
-              type="text"
-              placeholder="Demo"
-              name="demo"
-              id="demo"
-              value={demo}
-              onChange={handleInput}
-            />
-            <input
-              className="input"
-              type="text"
-              placeholder="Tecnologías"
-              name="technologies"
-              id="technologies"
-              value={technologies}
-              onChange={handleInput}
-            />
-            <textarea
-              className="textarea"
-              type="text"
-              placeholder="Descripción"
-              name="desc"
-              id="desc"
-              value={desc}
-              onChange={handleInput}
-            ></textarea>
-          </fieldset>
+            <fieldset className="buttons-img">
+              <input
+                className="btn"
+                type="button"
+                value="Subir foto de proyecto"
+              />
+              <input
+                className="btn"
+                type="button"
+                value="Subir foto de autora"
+              />
+            </fieldset>
 
-          <section className="ask-info">
-            <p className="subtitle">Cuéntanos sobre la autora</p>
-            <hr className="line" />
-          </section>
-
-          <fieldset className="autor">
-            <input
-              className="input"
-              type="text"
-              placeholder="Nombre"
-              name="autor"
-              id="autor"
-              value={autor}
-              onChange={handleInput}
-            />
-            <input
-              className="input"
-              type="text"
-              placeholder="Trabajo"
-              name="job"
-              id="job"
-              value={job}
-              onChange={handleInput}
-            />
-          </fieldset>
-
-          <section className="buttons-img">
-            <button className="btn">Subir foto de proyecto</button>
-            <button className="btn">Subir foto de autora</button>
-          </section>
-          <section className="buttons-img">
-            <button className="btn-large" onClick="{handleClickCreateCard}">
-              Crear Tarjeta
-            </button>
-          </section>
-
-          <section className="card">
-            <span className=""> La tarjeta ha sido creada: </span>
-            <a href="" className="" target="_blank" rel="noreferrer"> </a>
-          </section>
+            <fieldset className="buttons-img">
+              <input
+                className="btn-large"
+                onClick="{handleClickCreateCard}"
+                type="submit"
+                value="Crear Tarjeta"
+              />
+            </fieldset>
+          </form>
+          <div className="card">
+            <p className=""> La tarjeta ha sido creada: </p>
+            <a href="" className="" target="_blank" rel="noreferrer">
+            </a>
+          </div>
         </section>
       </main>
-    </div>;
+    </div>
+  );
 }
 
 export default App;
